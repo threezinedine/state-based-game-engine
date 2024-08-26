31060e537ac60677c5440ca776c8431d2a48fda4
threezinedine <threezinedine@gmail.com>
2024-08-26 19:41:05 +0700

    delete build

--- build/vendors/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Acer/Games Dev/state-based-game-engine/vendors/googletest/googletest
-# Build directory: C:/Users/Acer/Games Dev/state-based-game-engine/build/vendors/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
