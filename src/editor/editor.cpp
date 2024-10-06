747641c4bee43a9c24dab02cde905026affb4fe3
threezinedine <threezinedine@gmail.com>
2024-10-06 21:06:15 +0700

    fix log widget close bug

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -51,6 +51,7 @@ namespace ntt
 
         rlImGuiSetup(true);
         s_renderTexture = LoadRenderTexture(width, height);
+        s_openLog = FALSE;
     }
 
     void EditorRun()
@@ -89,6 +90,7 @@ namespace ntt
     {
         if (!s_useEditor)
         {
+            return;
         }
 
         if (s_shouldStart)
@@ -105,6 +107,22 @@ namespace ntt
         s_shouldStop = FALSE;
 
         rlImGuiBegin();
+
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("View"))
+            {
+                ImGui::MenuItem("Log", NULL, &s_openLog);
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
 
         ImGui::Begin("Viewport");
         ImVec2 size = ImGui::GetContentRegionAvail();
@@ -143,72 +161,75 @@ namespace ntt
             ImGui::ShowDemoWindow(&show);
         }
 
-        if (ImGui::Begin("Log", &s_openLog))
+        if (s_openLog)
         {
-            if (ImGui::BeginPopup("Options"))
+            if (ImGui::Begin("Log", &s_openLog))
             {
-                ImGui::Checkbox("Auto-scroll", &s_autoScroll);
-                ImGui::EndPopup();
-            }
-
-            if (ImGui::Button("Options"))
-            {
-                ImGui::OpenPopup("Options");
-            }
-
-            ImGui::SameLine();
-            b8 clear = ImGui::Button("Clear");
-            ImGui::SameLine();
-            b8 copy = ImGui::Button("Copy");
-            ImGui::SameLine();
-            s_filter.Draw("Filter", -100.0f);
-
-            ImGui::Separator();
-
-            if (ImGui::BeginChild("scrolling", ImVec2(0, 0), ImGuiChildFlags_None, ImGuiWindowFlags_HorizontalScrollbar))
-            {
-                if (clear)
-                    Clear();
-                if (copy)
-                    ImGui::LogToClipboard();
-
-                ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
-                const char *buf = s_buf.begin();
-                const char *buf_end = s_buf.end();
-                if (s_filter.IsActive())
+                if (ImGui::BeginPopup("Options"))
                 {
-                    for (int line_no = 0; line_no < s_lineOffsets.Size; line_no++)
-                    {
-                        const char *line_start = buf + s_lineOffsets[line_no];
-                        const char *line_end = (line_no + 1 < s_lineOffsets.Size) ? (buf + s_lineOffsets[line_no + 1] - 1) : buf_end;
-                        if (s_filter.PassFilter(line_start, line_end))
-                            ImGui::TextUnformatted(line_start, line_end);
-                    }
+                    ImGui::Checkbox("Auto-scroll", &s_autoScroll);
+                    ImGui::EndPopup();
                 }
-                else
+
+                if (ImGui::Button("Options"))
                 {
-                    ImGuiListClipper clipper;
-                    clipper.Begin(s_lineOffsets.Size);
-                    while (clipper.Step())
+                    ImGui::OpenPopup("Options");
+                }
+
+                ImGui::SameLine();
+                b8 clear = ImGui::Button("Clear");
+                ImGui::SameLine();
+                b8 copy = ImGui::Button("Copy");
+                ImGui::SameLine();
+                s_filter.Draw("Filter", -100.0f);
+
+                ImGui::Separator();
+
+                if (ImGui::BeginChild("scrolling", ImVec2(0, 0), ImGuiChildFlags_None, ImGuiWindowFlags_HorizontalScrollbar))
+                {
+                    if (clear)
+                        Clear();
+                    if (copy)
+                        ImGui::LogToClipboard();
+
+                    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+                    const char *buf = s_buf.begin();
+                    const char *buf_end = s_buf.end();
+                    if (s_filter.IsActive())
                     {
-                        for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
+                        for (int line_no = 0; line_no < s_lineOffsets.Size; line_no++)
                         {
                             const char *line_start = buf + s_lineOffsets[line_no];
                             const char *line_end = (line_no + 1 < s_lineOffsets.Size) ? (buf + s_lineOffsets[line_no + 1] - 1) : buf_end;
-                            ImGui::TextUnformatted(line_start, line_end);
+                            if (s_filter.PassFilter(line_start, line_end))
+                                ImGui::TextUnformatted(line_start, line_end);
                         }
                     }
-                    clipper.End();
+                    else
+                    {
+                        ImGuiListClipper clipper;
+                        clipper.Begin(s_lineOffsets.Size);
+                        while (clipper.Step())
+                        {
+                            for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
+                            {
+                                const char *line_start = buf + s_lineOffsets[line_no];
+                                const char *line_end = (line_no + 1 < s_lineOffsets.Size) ? (buf + s_lineOffsets[line_no + 1] - 1) : buf_end;
+                                ImGui::TextUnformatted(line_start, line_end);
+                            }
+                        }
+                        clipper.End();
+                    }
+                    ImGui::PopStyleVar();
+
+                    if (s_autoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+                    {
+                        ImGui::SetScrollHereY(1.0f);
+                    }
+                    ImGui::EndChild();
                 }
-                ImGui::PopStyleVar();
-
-                if (s_autoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
-                    ImGui::SetScrollHereY(1.0f);
-
-                ImGui::EndChild();
+                ImGui::End();
             }
-
-            ImGui::End();
         }
 
         rlImGuiEnd();
